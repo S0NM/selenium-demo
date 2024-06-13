80e66f149004b7a7cbaad2c3972113720be07761
elison <>
2024-06-13 20:30:19 +0700

    refactor code

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from bs4 import BeautifulSoup
 st.set_page_config(layout="wide")
 
 # Keep text only
-@st.cache_data(ttl=3600)
 def get_website_content(url):
     driver = None
     try:
@@ -22,30 +21,22 @@ def get_website_content(url):
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1920,1200')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                   options=options)
-        print(f"DEBUG:DRIVER:{driver}")
+        st.write(f"DEBUG:DRIVER:{driver}")
         driver.get(url)
-        time.sleep(10)
+        time.sleep(5)
         html_doc = driver.page_source
         driver.quit()
         soup = BeautifulSoup(html_doc, "html.parser")
         return soup.get_text()
     except Exception as e:
-        print(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
     finally:
         if driver is not None: driver.quit()
     return None
 
 
-def show_summary(article_json):
-    summary_html = f"""<div style="padding: 10px; border-radius: 5px;">
-                                        <p> Nội dung tóm tắt của bài viết: </p>
-                                        <h4 style="color: darkgreen;">{article_json["title"]}</h4>
-                                        <p> Ngày xuất bản:{article_json["publish_date"]}</p>
-                                        <p> {article_json["summary"]}</p>
-                                        </div>"""
-    st.markdown(summary_html, unsafe_allow_html=True)
 
 
 # ---------------- Page & UI/UX Components ------------------------
@@ -56,13 +47,13 @@ def main_sidebar():
 
 
 def site_extraction_page():
-    SAMPLE_URL = "https://www.vib.com.vn/vn/the-tin-dung/vib-financial-free"
+    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
     url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
 
     clicked = st.button("Load Page Content",type="primary")
     if clicked:
         with st.container(border=True):
-            with st.spinner("Đang tải nội dung website..."):
+            with st.spinner("Loading page website..."):
                 content = get_website_content(url)
                 st.write(content)
 
